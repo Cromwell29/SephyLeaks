09efbf3b6ad1a2d943667545ac4b654870ed8452
Cromwell29 <62707591+Cromwell29@users.noreply.github.com>
2025-04-28 14:18:43 +0200

    Update kofi.js

--- js/kofi.js
+++ js/kofi.js
@@ -3,13 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("kofi-container");
     if (container) {
       clearInterval(checkFooterInterval);
-      const script = document.createElement("script");
-      script.src = "https://storage.ko-fi.com/cdn/widget/Widget_2.js";
-      script.onload = () => {
-        kofiwidget2.init('Aidez nous sur SephyLeaks', '#c573f5', 'Y8Y41E6NZ8');
-        kofiwidget2.draw();
-      };
-      container.appendChild(script);
+
+      if (typeof kofiwidget2 === "undefined") {
+        const script = document.createElement("script");
+        script.src = "https://storage.ko-fi.com/cdn/widget/Widget_2.js";
+        script.onload = () => {
+          kofiwidget2.init('Aidez nous sur SephyLeaks', '#c573f5', 'Y8Y41E6NZ8');
+          kofiwidget2.draw();
+        };
+        container.appendChild(script);
+      }
     }
-  }, 100); // Vérifie toutes les 100ms jusqu'à ce que le footer soit chargé
+  }, 100);
 });
